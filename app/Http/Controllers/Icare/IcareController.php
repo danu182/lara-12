51aeb3b3cfdbea2190dc73050abd169c5ef9dda8
danu182 <danu182.182@gmail.com>
2025-05-08 17:00:33 +0700

    buat controller Icare baru

---
+++ app/Http/Controllers/Icare/IcareController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Icare;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IcareController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function hasil()
+    {
+        
+    }
+
+}
